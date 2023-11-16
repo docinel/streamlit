fbfdf257812feaf7ab605c9c04fca90f32e9f360
docinel <docinel@gmail.com>
2023-11-16 01:20:08 -0300

    Algumas config

--- dependencia.py
+++ dependencia.py
@@ -16,18 +16,86 @@ def insert_user(email, username, password):
 
     return db.put({'email': email, 'username': username, 'password': password})
 
-insert_user("docinel@gmail.com", "docinel", "123456")
+# insert_user("docinel@gmail.com", "docinel", "123456")
 
 def fetch_all_users():
     res = db.fetch()
     return res.items
 
-def sign_uop():
+# print(fetch_all_users())
+
+
+    
+def get_user_by_email():
+    users = db.fetch()
+    emails = []
+    for user in users.items:
+        emails.append(user['key'])
+    return emails
+
+# print(get_user_by_email())
+
+def get_user_by_username():
+    users = db.fetch()
+    usernames = []
+    for user in users.items:
+        usernames.append(user['key'])
+    return usernames
+
+# print(get_user_by_username())
+
+
+def validate_email(email):
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex,email)):
+        return True
+    else:
+        return False
+
+def validate_username(username):
+    regex = '^[a-zA-Z0-9]+$'
+    if(re.search(regex,username)):
+        return True
+    else:
+        return False
+
+
+def sign_up():
     with st.form("sign_up", clear_on_submit=True):
         st.subheader(":green[Sign Up]") 
-        email = st.text_input("Email", placeholder="Enter your email")
-        username = st.text_input("Username", placeholder="Enter your username")
-        password = st.text_input("Password", type="password", placeholder="Enter your password")
-        confirm_password = st.text_input("Confirm Password", type="password", placeholder="Confirm your password")
-        submit = st.form_submit_button(label="Sign Up")
-    
+        email = st.text_input(":blue[Email]", placeholder="Enter your email")
+        username = st.text_input(":blue[Username]", placeholder="Enter your username")
+        password1 = st.text_input(":blue[Password]", type="password", placeholder="Enter your password")
+        password2 = st.text_input(":blue[Confirm Password]", type="password", placeholder="Confirm your password")
+        
+        if email:
+           if validate_email(email):
+               if email not in get_user_by_email():
+                   if validate_username(username):
+                       if username not in get_user_by_username():
+                           if len(username) >= 2:
+                               if len(password1) >= 6:
+                                   if password1 == password2:
+                                       # Add User to DB
+                                       hashed_password = stauth.Hasher([password2]).generate()
+                                       insert_user(email, username, hashed_password[0])
+                                       st.success('Account created successfully!!')
+                                       st.balloons()
+                                   else:
+                                       st.warning('Passwords Do Not Match')
+                               else:
+                                   st.warning('Password is too Short')
+                           else:
+                               st.warning('Username Too short')
+                       else:
+                           st.warning('Username Already Exists')
+                   else:
+                       st.warning('Invalid Username')
+               else:
+                   st.warning('Email Already exists!!')
+           else:
+               st.warning('Invalid Email')
+        btn1, bt2, btn3, btn4, btn5 = st.columns(5)
+
+        with btn3:
+            st.form_submit_button('Sign Up')
---
+++ main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from dependencia import sign_up, fetch_all_users
+
+st.set_page_config(page_title="Sign Up", page_icon=":green_book:", initial_sidebar_state="collapsed")
+
+try:
+    users = fetch_all_users()
+    emails = []
+    usernames = []
+    passwords = []
+
+    for user in users:
+        emails.append(user['key'])
+        usernames.append(user['username'])
+        passwords.append(user['password'])
+
+    credentials = {
+        'usernames': {}
+    }
+
+    for index in range(len(emails)):
+        credentials['usernames'][usernames[index]] = {
+            'email': emails[index],
+            'password': passwords[index]
+        }
+
+    authenticator = stauth.Authenticate(credentials, 'myapp', 'abcdef', cookie_expiry_days=4)
+
+    email, authentication_status, username = authenticator.login(':green[Login]', 'main')
+
+    info, info1 = st.columns(2)
+    
+    if not authentication_status:
+        sign_up()
+
+    if username:
+        if username in usernames:
+            if authentication_status:
+                st.subheader('This is home page')
+                st.markdown("""
+                Created by:
+                            - [Rodrigo Docinel](https://github.com/rodrigodocinel)                
+""")
+                st.sidebar(f"Welcome {username}")
+                authenticator.logout('Logout', 'sidebar')
+            elif not authentication_status:
+                with info:
+                    st.error('Incorrect Password or username')
+            else:
+                with info:
+                    st.warning('Please feed in your credentials')
+        else:
+            with info:
+                st.warning('Username does not exist, Please Sign up')
+
+
+except:
+    st.success('Refresh Page')
